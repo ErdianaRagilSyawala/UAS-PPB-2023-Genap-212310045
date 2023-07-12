3ea7128701aec8cfec0a80135e38f0d881c4f6a3
ErdianaRagilSyawala <erdiana1712@gmail.com>
2023-07-12 22:20:50 +0700

    test

---
+++ App.js
@@ -0,0 +1,30 @@
+
+import DaftarMenu from "./src/component/Homes/DaftarMenu";
+import Home from "./src/component/Home";
+import LoadingPage from "./src/component/LoadingPage";
+import Login from "./src/component/Login";
+
+// import Login from "./src/component/Login";
+
+
+export default function App() {
+  return (
+
+        // <LoadingPage/>
+        <DaftarMenu/>
+
+//     <View style={styles.container}>
+//       <Text>Open up App.js to start working on your app!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+  );
+}
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
---
+++ src/component/Home.js
@@ -0,0 +1,78 @@
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <View style={styles.container}>
+      <Image source={require("../../assets/MieAyam.jpeg")} 
+       style={{width: 120, height: 120, marginTop: 100, borderRadius: 100, }} />
+        <Text style={styles.text}>SELAMAT DATANG</Text>
+        <Text style={styles.text2}>OWNER BAKMIE JOKO</Text>
+        <Text style={styles.button1}>DAFTAR PRODUK</Text>
+        <Text style={styles.button3}>LAPORAN</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create ({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    padding: 20,
+    backgroundColor: '#EE8C33',
+  },
+  text: {
+    textAlign:'center',
+    fontWeight:'bold',
+    fontSize: 25,
+    color: 'white',
+  },
+  text2: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 25,
+    color: '#E2AFAF'
+  },
+  button1: {
+    backgroundColor: 'white',
+    width: '100%',
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 100,
+    borderRadius: 15,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 20,
+    paddingTop: 10,
+  },
+  button2: {
+    backgroundColor: 'white',
+    width: '100%',
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 20,
+    borderRadius: 15,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 20,
+    paddingTop: 10,
+  },
+  button3: {
+    backgroundColor: 'white',
+    width: '100%',
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 20,
+    borderRadius: 15,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 20,
+    paddingTop: 10,
+  }
+});
+
+export default Home
